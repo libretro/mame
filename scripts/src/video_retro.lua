748d7f19e23ecc685536b37929b4f17e457a18cd
sonninnos <tony@pihaus.org>
2026-01-31 01:29:34 +0200

    Subtarget updates

--- scripts/src/video_retro.lua
+++ scripts/src/video_retro.lua
@@ -1,18 +1,14 @@
--- cv1k/ep1c12 subtarget hack
+-- cv1k subtarget hack
 if _OPTIONS["subtarget"]=="arcade" then
 	files {
-		MAME_DIR .. "src/mame/cave/ep1c12.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12.h",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit0.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit1.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit2.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit3.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit4.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit5.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit6.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit7.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12_blit8.cpp",
-		MAME_DIR .. "src/mame/cave/ep1c12in.ipp",
-		MAME_DIR .. "src/mame/cave/ep1c12pixel.ipp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit0.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit1.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit2.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit3.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit4.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit5.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit6.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit7.cpp",
+		MAME_DIR .. "src/mame/cave/cv1k_v_blit8.cpp",
 	}
 end
